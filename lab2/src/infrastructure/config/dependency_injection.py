cd383aad07f61a23572ae0f879a5c1401eb834fc
Margo250 <145214003+Margo250@users.noreply.github.com>
2026-04-23 22:28:41 +0300

    Update dependency_injection.py

--- lab2/src/infrastructure/config/dependency_injection.py
+++ lab2/src/infrastructure/config/dependency_injection.py
@@ -1,1 +1,16 @@
+from ...application.service.announcement_service import AnnouncementService
+from ..adapter.out.in_memory_announcement_repository import InMemoryAnnouncementRepository
+from ..adapter.out.console_notification_service import ConsoleNotificationService
 
+class DependencyContainer:
+    def __init__(self):
+        self.announcement_repository = InMemoryAnnouncementRepository()
+        self.notification_service = ConsoleNotificationService()
+        
+        self.announcement_service = AnnouncementService(
+            repository=self.announcement_repository,
+            notification_service=self.notification_service
+        )
+    
+    def get_announcement_service(self) -> AnnouncementService:
+        return self.announcement_service
