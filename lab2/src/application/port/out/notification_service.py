e64a53d1fc15ccfabe6b2cb6d5976118ff37867a
Margo250 <145214003+Margo250@users.noreply.github.com>
2026-04-23 22:26:14 +0300

    Update notification_service.py

--- lab2/src/application/port/out/notification_service.py
+++ lab2/src/application/port/out/notification_service.py
@@ -1,1 +1,6 @@
+from abc import ABC, abstractmethod
 
+class NotificationService(ABC):
+    @abstractmethod
+    def send_announcement_published(self, announcement_id: str, group_id: str) -> None:
+        pass
